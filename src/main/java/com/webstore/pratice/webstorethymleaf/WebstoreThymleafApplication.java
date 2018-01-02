19aff1ddab54d128834fa99bf779fa2e4f9a4d0b
Syed Affan Hamdani <syedaffanhamdani@bitbucket.org>
2018-01-02 14:54:21 +0100

    initial commit

---
+++ src/main/java/com/webstore/pratice/webstorethymleaf/WebstoreThymleafApplication.java
@@ -0,0 +1,12 @@
+package com.webstore.pratice.webstorethymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstoreThymleafApplication.class, args);
+	}
+}
---
+++ src/test/java/com/webstore/pratice/webstorethymleaf/WebstoreThymleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webstore.pratice.webstorethymleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebstoreThymleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
